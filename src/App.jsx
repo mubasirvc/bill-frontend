d0caf77342ebfb5d580d14fffe92a4bb0488388b
mubasir umbi <mubasirumbi@gmail.com>
2024-09-15 00:01:03 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import { Box } from "@mui/material";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";  // Import necessary components
-import Home from "./pages/home";
 import Items from "./pages/Items";
+import Home from "./pages/Home";
 import Category from "./pages/Category";
 import Navbar from "./components/Navbar";
-import GenerateBill from "./components/GenerateBill";
 
 function App() {
   return (
@@ -15,7 +14,6 @@ function App() {
           <Route path="/" element={<Home />} /> 
           <Route path="/items" element={<Items />} /> 
           <Route path="/category" element={<Category />} />
-          <Route path="/pdf" element={<GenerateBill />} />
         </Routes>
       </Box>
     </Router>
